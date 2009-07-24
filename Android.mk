88d099f8d084ee3a4ca6bf69ed5d79087cadd10d
Mike Lockwood <lockwood@android.com>
2009-07-24 15:46:24 -0400

    msm7k: Include librpc in qsd8k builds.
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- Android.mk
+++ Android.mk
@@ -18,5 +18,5 @@ ifeq ($(TARGET_BOARD_PLATFORM),msm7k)
 endif
 
 ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
-  include $(call all-named-subdir-makefiles,libcopybit libaudio)
+  include $(call all-named-subdir-makefiles,libcopybit libaudio librpc)
 endif
