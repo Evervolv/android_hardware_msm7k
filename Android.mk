f86e06e6df690a9bd66ff88487d71a4363f584b8
Dima Zavin <dima@android.com>
2010-05-17 21:49:02 -0700

    msm7k: build lights for 7x30 also
    Change-Id: I7f2f43caeac34c675765d7b6e83016288a9f61ed
    Signed-off-by: Dima Zavin <dima@android.com>

--- Android.mk
+++ Android.mk
@@ -17,7 +17,7 @@
 common_msm_dirs := libcopybit liblights libopencorehw librpc libstagefrighthw
 msm7k_dirs := $(common_msm_dirs) boot libgralloc libaudio
 qsd8k_dirs := $(common_msm_dirs) libgralloc-qsd8k libaudio-qsd8k dspcrashd
-msm7x30_dirs := libgralloc-qsd8k
+msm7x30_dirs := liblights libgralloc-qsd8k
 
 ifeq ($(TARGET_BOARD_PLATFORM),msm7k)
   include $(call all-named-subdir-makefiles,$(msm7k_dirs))
