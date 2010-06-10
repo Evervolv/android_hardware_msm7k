6688eab51d3d61e77e93c13d1525b4fd38dec0ae
Chih-Chung Chang <chihchung@google.com>
2010-06-10 13:32:51 +0800

    Change camera interface to support multiple cameras.
    Change-Id: I32fa5615b7fcd4caba34283d4b252bac478aa1eb

--- libcamera/QualcommCameraHardware.cpp
+++ libcamera/QualcommCameraHardware.cpp
@@ -1078,7 +1078,24 @@ namespace android {
         return mParameters;
     }
 
-    extern "C" sp<CameraHardwareInterface> openCameraHardware()
+    static CameraInfo sCameraInfo[] = {
+        {
+            CAMERA_FACING_BACK,
+            90,  /* orientation */
+        }
+    };
+
+    extern "C" int HAL_getNumberOfCameras()
+    {
+        return sizeof(sCameraInfo) / sizeof(sCameraInfo[0]);
+    }
+
+    extern "C" void HAL_getCameraInfo(int cameraId, struct CameraInfo* cameraInfo)
+    {
+        memcpy(cameraInfo, &sCameraInfo[cameraId], sizeof(CameraInfo));
+    }
+
+    extern "C" sp<CameraHardwareInterface> HAL_openCameraHardware(int cameraId)
     {
         LOGV("openCameraHardware: call createInstance");
         return QualcommCameraHardware::createInstance();
