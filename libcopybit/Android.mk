8338c01276b39870a361b0f4313a4bcd0f4a45e4
Iliyan Malchev <malchev@google.com>
2011-03-14 14:02:15 -0700

    hardware/msm7k: remove LOCAL_PRELINK_MODULE
    Change-Id: I4773482f4c358f6364a528b22964b4e036ac86a1
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- libcopybit/Android.mk
+++ libcopybit/Android.mk
@@ -14,12 +14,12 @@
 
 
 LOCAL_PATH:= $(call my-dir)
-# HAL module implemenation, not prelinked and stored in
+# HAL module implemenation stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 
 ifeq ($(TARGET_BOARD_PLATFORM),msm7k)
 include $(CLEAR_VARS)
-LOCAL_PRELINK_MODULE := false
+
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_SRC_FILES := copybit.cpp
@@ -31,7 +31,7 @@ endif
 
 ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
 include $(CLEAR_VARS)
-LOCAL_PRELINK_MODULE := false
+
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_SRC_FILES := copybit.cpp
