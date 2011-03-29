509190adcfed0d425d4f55e1dc02a7213ea0e0a4
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2011-03-29 18:48:31 +0100

    librpc: Don't mask lower bytes on program version 0x00020002
    It's used by a number of HTC devices (Aria, Slide, and Legend), and
    needs to be accessible in order to use loc_api
    
    Change-Id: I4ace3e4484b6ad93f0dfee6c06ecd2acb637acbc

--- librpc/clnt.c
+++ librpc/clnt.c
@@ -686,7 +686,7 @@ CLIENT *clnt_create(
         char name[20];
 
         /* for versions like 0x00010001, only compare against major version */
-        if ((vers & 0xFFF00000) == 0)
+        if (vers!=0x20002 && (vers & 0xFFF00000) == 0)
             vers &= 0xFFFF0000;
 
         pthread_mutex_lock(&rx_mutex);
